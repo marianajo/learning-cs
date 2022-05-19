d11a058609cc5771a9f38439ac6fb92f50db4665
Mariana Jó <mariana.jsouza@gmail.com>
2022-05-18 22:51:51 -0300

    add Hello World for OpenMP on parallel
    OpenMP is an API for parallel programming. This code is known as the
    "Hello World" for OpenMP, and it basically prints the corresponding
    thread number for each print statement, which is run by different
    threads each time.

---
+++ parallel/openmp/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[])
+{
+    int id;
+    #pragma omp parallel private(id)
+    {
+        id = omp_get_thread_num();
+        printf("%d: Hello World!\n", id);
+    }
+
+    return 0;
+}
